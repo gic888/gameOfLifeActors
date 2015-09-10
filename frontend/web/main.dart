dd5220e8f4ea46586946e250d80a27c13ff15b7a
Graham Cummins <grahamiancummins@gmail.com>
2015-09-09 22:15:12 -0600

    Websocket

--- frontend/web/main.dart
+++ frontend/web/main.dart
@@ -1,8 +1,52 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'dart:html';
+import 'dart:async';
+
+WebSocket ws;
+
+outputMsg(String msg) {
+  var output = querySelector('#output');
+  var text = msg;
+  if (!output.text.isEmpty) {
+    text = "${output.text}\n${text}";
+  }
+  output.text = text;
+}
+
+void initWebSocket([int retrySeconds = 2]) {
+  var reconnectScheduled = false;
+
+  outputMsg("Connecting to websocket");
+  ws = new WebSocket('ws://localhost:8888');
+
+  void scheduleReconnect() {
+    if (!reconnectScheduled) {
+      new Timer(new Duration(milliseconds: 1000 * retrySeconds), () => initWebSocket(retrySeconds * 2));
+    }
+    reconnectScheduled = true;
+  }
+
+  ws.onOpen.listen((e) {
+    outputMsg('Connected');
+    ws.send('Hello from Dart!');
+  });
+
+  ws.onClose.listen((e) {
+    outputMsg('Websocket closed, retrying in $retrySeconds seconds');
+    scheduleReconnect();
+  });
+
+  ws.onError.listen((e) {
+    outputMsg("Error connecting to ws");
+    scheduleReconnect();
+  });
+
+  ws.onMessage.listen((MessageEvent e) {
+    outputMsg('Received message: ${e.data}');
+  });
+}
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  initWebSocket();
 }
